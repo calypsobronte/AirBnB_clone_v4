93e004f428887500b3b95a59bcbd666fc534bfd4
Jormao <jormao@gmail.com>
2020-05-17 20:48:27 +0000

    fix tasks 4

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
     }
     $('.amenities h4').text(Object.values(NewAmenity).join(', '));
   });
-  $.get('http://0.0.0.0:5001/api/v1/status/', (data, status) => {
+  $.get('http://localhost:5001/api/v1/status/', (data, status) => {
     if (data.status === 'OK') {
       $('DIV#api_status').addClass('available');
     } else {
@@ -17,13 +17,36 @@ $(document).ready(function () {
   });
   $.ajax({
     type: 'POST',
-    url: 'http://0.0.0.0:5001/api/v1/places_search/',
+    url: 'http://localhost:5001/api/v1/places_search/',
     data: '{}',
     dataType: 'json',
     contentType: 'application/json',
-    success: function (places) {
-      for (let place of places) {
-        $('section.places').append('<article><div class="title_box"><h2>'+ place.name + '</h2><div class="price_by_night">' + place.price_by_night + '</div></div><div class="information">' + '<div class="max_guest">' + place.max_guest + 'Guest' + (place.max_guest ? 1 : 's') + '</div>' + '<div class="number_rooms">' + place.number_rooms + 'Bedroom' + (place.number_rooms ? 1 : 's') + '</div>' +  '<div class="number_bathrooms">' + place.number_bathrooms + 'Bathroom' + (place.number_bathrooms ? 1 : 's') + '</div></div>' + ' <div class="user">' + '<b>Owner:</b>' + place.user.first_name + place.user.last_name + '</div>' + '<div class="description">' + place.description + '</div></article>');
+    success: function (info) {
+      for (const place of info) {
+        $('section.places').append(
+          '<article><div class="title_box"><h2>' +
+          place.name +
+          '</h2><div class="price_by_night">' +
+          place.price_by_night +
+          '</div></div><div class="information">' +
+          '<div class="max_guest">' +
+          place.max_guest +
+          'Guest' +
+          (place.max_guest ? 1 : 's') +
+          '</div>' +
+          '<div class="number_rooms">' +
+          place.number_rooms +
+          'Bedroom' +
+          (place.number_rooms ? 1 : 's') +
+          '</div>' +
+          '<div class="number_bathrooms">' +
+          place.number_bathrooms +
+          'Bathroom' +
+          (place.number_bathrooms ? 1 : 's') +
+          '</div></div>' +
+          '<div class="description">' +
+          place.description +
+          '</div></article>');
       }
     }
   });
