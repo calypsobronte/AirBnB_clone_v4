61e6a3f12bcec7c0fc97bcc6514a2736cbccada8
Jormao <jormao@gmail.com>
2020-05-17 03:13:47 +0000

    task 3

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -8,9 +8,12 @@ $(document).ready(function () {
         }
         $('.amenities h4').text(Object.values(new_amenity).join(', '));
     });
-    $.get('http://0.0.0.0:5001/api/v1/status/', (data) => {
-        if (data.status === 'success') {
+    $.get('http://localhost:5001/api/v1/status/', (data, status) => {
+        if (data.status === 'OK') {
             $('DIV#api_status').addClass('available');
-        }
+        } else {
+			$('DIV#api_status').removeClass('available');
+		}
     });
-});
+});	
+	
